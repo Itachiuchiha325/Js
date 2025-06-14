2e86eef8862416f6c1d7a33aad3392a4c3b74dd1
Yash Pandit <125740864+Itachiuchiha325@users.noreply.github.com>
2025-06-14 04:01:57 +0000

    Disscuss About Converstions

---
+++ 01_Basics/03_ConversionOperations.js
@@ -0,0 +1,27 @@
+let score = "yash"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let ValueInNumber = Number (score);
+console.log(typeof ValueInNumber)
+console.log( ValueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+//"true" "false" = 1 0 
+
+let isLoggedIn = 0
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true false=> 0
+// yash => true
+
+let somenumber = 33;
+let stringnumber = String (somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber)
+
+    
