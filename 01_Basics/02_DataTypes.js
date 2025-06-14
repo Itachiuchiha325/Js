29ef0c0452c3e0af7fd1ffaafe9e91f15ebf0cb2
Yash Pandit <125740864+Itachiuchiha325@users.noreply.github.com>
2025-06-14 04:01:25 +0000

    Discussed about DataTypes

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version
+
+// alert("yash") cant run cause we are using Nodejs not browser 
+
+console.log(3+2);
+
+let number = 45;
+let string = "yash";
+let islogged = true;
+
+
+
+//number => 2 power 52
+//bigint 
+//string => " "
+//boolean => true false              --------------    primitive
+//null => standalone value
+//undefined =>
+//symbol  => unique   
+
+console.log(typeof islogged) //use to determine the data types category
+console.log(typeof null) // its object which is not
+console.log(type);
+//object
+
