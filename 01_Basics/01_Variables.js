55c575377f8c85106d1dc63230246736a1ef0cf7
Yash Pandit <125740864+Itachiuchiha325@users.noreply.github.com>
2025-06-14 04:00:57 +0000

    Discussed about variables

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,16 @@
+const account_Id = 4032004;
+let account_Email = "pandityash2004@gmail.com";
+var account_Password = "9356007931";
+account_city = "Boisar"; 
+account_State; // undefined
+
+// account_Id = 9; Not allowed  
+
+// account_Email = "yash@gmail.com"  Allowed
+// account_Password = "peq64sdwfg"
+// account_city = "Pune"
+
+/* 
+    Preffered not to use var because of issue in log scope and functional scope
+*/
+console.table([account_Email,account_Id,account_Password,account_city]);
